4441655c0568b086a8bba35b3df1936040c3bff0
Brad Jones <48833643+bjonesPew@users.noreply.github.com>
2019-08-20 15:55:12 -0400

    Create spacedBar.R

---
+++ plotting_functions/spacedBar.R
@@ -0,0 +1,151 @@
+spacedBar <- function(list,	###input list to plot
+	xmin = -.18,		###xmin for plot (xmax is determined from input)
+	col.vec, 			###colors to plot
+	val.lab, 			###value labels
+	res = 1, 			###resolution for plot
+	plot.width = 3.2,		###width of plot window in inches
+	write.file = "no", 	###write out a file? "pdf" or "jpg"
+	bar.width = .5,		###width of bars (determines the height of the plot)
+	n.cats = 4, 		###number of categories to plot (to exclude DK/Refused mostly)
+	col.lab = NULL, 		###column labels
+	spaces = NULL) {		###supply spaces between bars (should be vector of length n.cats-1) (otherwise set to 0.05)
+
+	##set up the height of the box based on the number of elements
+	##(in reverse order; list should be ordered with total first, etc)
+	ymax <- ifelse(length(col.lab)>0, -.5, .5)
+	if (length(col.lab)>0) {
+		i <- grep("\\n", col.lab)
+		if (length(i) > 0) ymax = -.75
+	}
+
+	ylim <- c(length(list)+.5, ymax)
+
+
+	##open the plot window (multiplied by the resolution factor)
+	dev.new(width=plot.width*res, height=
+		length(list)*bar.width*res)
+
+##save to file
+if (write.file != "no") {
+
+if (write.file == "jpg") {
+	src <- tempfile(fileext = ".jpg")
+	jpeg(src, width = plot.width, 
+		height = bar.width*length(list),
+		units = 'in', res = 1000)
+}
+if (write.file == "pdf") {
+	src <- tempfile(fileext = ".pdf")
+	pdf(src, width = plot.width, 
+		height = bar.width*length(list))
+}
+
+}
+
+##find get the spacers
+mat <- array(NA, c( length(list), n.cats))
+for (j in 1:length(list)) {
+	if (length(list[[j]]) == 0) next
+	for (k in 1:n.cats) {
+		mat[j,k] <- list[[j]][k]
+	}
+}
+space <- array(NA, c( length(list), n.cats - 1))
+
+if (is.null(spaces)) {
+for (j in 1:(n.cats - 1)) {
+	space[,j] <- max(mat[,j], na.rm = TRUE) - mat[,j] + .05
+}
+}
+
+if (!is.null(spaces)) {
+for (j in 1:(n.cats - 1)) {
+	space[,j] <- max(mat[,j], na.rm = TRUE) - mat[,j] + spaces[j]
+}
+}
+
+xmax <- max( rowSums(mat) + rowSums(space), na.rm = TRUE )
+xlim <- c(xmin, xmax)
+	par(mar = rep(.1, 4))
+	plot(0,0, pch = '', xlim = xlim,
+		ylim = ylim, axes = FALSE,
+		xlab = '', ylab = '')
+
+
+	x.pos <- 0
+	pos <- rep(NA, 3)
+	##Add column labels
+	if (length(col.lab)>0) {
+		for (j in 1:length(col.lab)) {
+			pos[j] <- x.pos
+			fam = ifelse(write.file == "pdf", "", "demi")
+			text(x.pos + (max(mat[,j], na.rm = TRUE)/2), -.25,
+				col.lab[j], family = fam, col = col.vec[j],
+				cex = .75)
+			if (is.null(spaces)) x.pos <- 
+				x.pos + max(mat[,j], na.rm = TRUE) +.05
+			if (!is.null(spaces)) x.pos <- 
+				x.pos + max(mat[,j], na.rm = TRUE) + spaces[j]
+		}
+	}
+
+	##run through the list to add the bars
+	for (j in 1:length(list)) {
+		fam = ifelse(write.file == "pdf", "", "fgb")
+		##skip NULL entries
+		if (length(list[[j]]) == 0) {
+			
+			text(xlim[1], j, names(list)[[j]], pos = 4, 
+				family = fam, offset = -.75,
+				font = 3,
+				cex = .75)
+			next
+		}
+		##add label
+		text(0, j, names(list)[[j]], pos = 2, 
+                family = fam,
+                cex = .75)
+	x = 0
+	for (k in 1:n.cats) {
+		 
+		addBar2(x = x, val = list[[j]][k], 
+			col = col.vec[k], 
+			val.lab = list[[j]][k] > 0,
+			pos = j, shiftSmall = TRUE, write.file = write.file)
+		x = x + mat[j,k]
+		if (k < n.cats) x = x + space[j,k]
+	}
+	}
+
+if (write.file != "no") {
+	dev.off()
+	dev.off()
+	return(src)
+}
+}
+
+addBar2 <- function(x, y, val, col, val.lab = TRUE,
+	pos = 1, shiftSmall = FALSE, write.file) {
+	
+	fam = ifelse(write.file == "pdf", "", "fgb")
+	polygon( x = c(x, x+val, x+val, x),
+		y = c(pos -.4, pos-.4, pos+.4, pos+.4),
+		border = NA, col = col)
+
+                ## write value labels
+		if (!shiftSmall) {
+		
+                if (val.lab & abs(val) > 0) text(x + val/2, 
+                        pos, abs(round(val*100)),
+                        family = fam,
+                        cex = .75)
+		}
+
+		if (shiftSmall) {
+			shift <- ifelse(val*100 < 3, .05, 0)
+                if (val.lab) text(x + val/2 + shift, 
+                        pos, abs(round(val*100)),
+                        family = fam,
+                        cex = .75)
+		}
+}
