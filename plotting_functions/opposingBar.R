4c038b1c582d3dfb36dee2a9118dff3dfed6c29e
Brad Jones <48833643+bjonesPew@users.noreply.github.com>
2019-10-15 16:34:19 -0400

    Update opposingBar.R

--- plotting_functions/opposingBar.R
+++ plotting_functions/opposingBar.R
@@ -83,7 +83,7 @@ if (addNets & n.cats == 2) {
 	if (length(col.lab)>0) {
 		for (j in 1:length(col.lab)) {
 			fam = ifelse(write.file == "pdf", "", "demi")
-			text(lab.pos[j], -.2,
+			text(lab.pos[j], -.2-.2*length(lines),
 				col.lab[j], family = fam, col = col.vec[j],
 				cex = .75)
 		}
